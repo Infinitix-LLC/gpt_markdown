90bc9e8ed8f2549744803c5c9327c0690b3ab712
Samin Yeasar Sohag <sssohag172@gmail.com>
2026-08-17 12:45:14 +0600

    Gpt Markdown Chat Page Adapter Implemented

--- lib/gpt_chat/data/services/id_generator.dart
+++
@@ -1,9 +0,0 @@
-/// Monotonic ids, unique within a process run. No external uuid dependency.
-class IdGenerator {
-  int _counter = 0;
-
-  String next([String prefix = 'id']) {
-    _counter++;
-    return '$prefix-${DateTime.now().microsecondsSinceEpoch}-$_counter';
-  }
-}
